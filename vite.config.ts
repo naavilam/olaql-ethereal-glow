a7ce20177adb0c5881561ce5b5768801d518c56f
Nara Avila <naraavilamoraes@gmail.com>
2025-04-19 11:50:57 -0300

    Corrige alias '@' e rebuild com base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import react from '@vitejs/plugin-react'
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+
+
+export default defineConfig({
+  base: '/olaql-ethereal-glow/',
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+})
